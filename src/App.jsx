69b650d91b25237831a3a638dcda85d41a214b77
johnvdpk <johnvdpk@gmail.com>
2023-09-14 11:50:49 +0200

    de css is niet mooi maar idee snap ik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const[nameValue, setNameValue] = useState('');
+    const[ageValue, setAgeValue] = useState('');
+    const [termsCondi, toggleTerms] =useState(false);
+
     return (
-        <div>
-            Maak hier jouw formulier!
-        </div>
+        <>
+            <form>
+                <label>Naam:
+                <input
+
+                    type="text"
+                    id="form-message"
+                    name="message"
+                    placeholder="Wat is je naam?"
+                    value={nameValue}
+                    onChange={(e)=>setNameValue(e.target.value)}
+
+                />
+                </label>
+                <label>Leeftijd:
+                <input
+                    type=""
+                    id="form-message"
+                    name="leeftijd"
+                    placeholder="wat is je leeftijd"
+                    value={ageValue}
+                    onChange={(e)=>setAgeValue(e.target.value)}
+                    />
+                </label>
+                <label>checkbox
+                    <input
+                        type='checkbox'
+                        name='checkbox'
+                        checked={termsCondi}
+                        onChange={()=> toggleTerms(!termsCondi)}
+                        />
+                    <p>Ik ga akkoord</p>
+                </label>
+
+
+            </form>
+        </>
     );
 }
 
